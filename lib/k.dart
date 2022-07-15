a9c6a8aa78f3e97c8f296cc98d04dd43b577e65f
Petar Jovanovic <jovanovic.petar499@gmail.com>
2022-07-16 00:22:46 +0200

    Extracted constants file

---
+++ lib/k.dart
@@ -0,0 +1,25 @@
+class K {
+  static const appName = "Exchangeagram";
+
+  static const fontFamilyVegan = "Vegan";
+  static const fontFamilyRoboto = 'Roboto';
+  static const fontFamilyIbarra = 'IbarraRealNova';
+
+  static const appLogo = "assets/images/exchangeagram.png";
+  static const testimonial = "A Billion dollar idea...";
+
+  static const heroText = "What if you could take a quick photo and post it directly";
+  static const secondHeroText = "Well look no further, because we have the perfect solution for you.";
+  static const online = 'on the line';
+
+  static const trailingText = "Add your friends and create and exchange.";
+
+  static const seeUsers = "See who is already exchanging photos";
+  static const usersButton = "See all users";
+
+  static const heroImage = "assets/images/hero.jpeg";
+
+  static const testimonialAvatar = 'assets/images/intern.jpeg';
+  static const testimonialName = "Billy McMahon";
+  static const testimonialWork = "Intern at Google";
+}
