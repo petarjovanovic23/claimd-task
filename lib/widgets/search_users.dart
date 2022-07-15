87990a982584d0b0520fe7b77ab4355c2ee7d3ea
Petar Jovanovic <jovanovic.petar499@gmail.com>
2022-07-16 01:12:43 +0200

    Started layouting the search widgets section

--- lib/widgets/search_users.dart
+++ lib/widgets/search_users.dart
@@ -1,3 +1,4 @@
+import 'package:claimd_task/k.dart';
 import 'package:flutter/material.dart';
 
 class SearchUsers extends StatelessWidget {
@@ -5,8 +6,25 @@ class SearchUsers extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("Users"),
+    TextStyle mainStyle = Theme.of(context).textTheme.headline3!;
+    TextStyle headline = Theme.of(context).textTheme.headline4!;
+    return Column(
+      children: [
+        Text(K.trailingText, style: headline),
+        Text(K.seeUsers, style: mainStyle),
+        ElevatedButton(
+          // style: ButtonStyle(
+          //   shape: MaterialStateProperty.all(RoundedRectangleBorder(borderRadius: BorderRadius.circular(18.0))),
+          //   padding: MaterialStateProperty.all(const EdgeInsets.symmetric(vertical: 24.0, horizontal: 48.0)),
+          //   backgroundColor: MaterialStateProperty.all(Colors.black26),
+          // ),
+          onPressed: () {},
+          child: const Text(
+            K.usersButton,
+            style: TextStyle(color: Colors.white70, fontSize: 24, fontFamily: K.fontFamilyIbarra),
+          ),
+        ),
+      ],
     );
   }
 }
