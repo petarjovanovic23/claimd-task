ff1a9aecc5f49f5c2c4b706ade938fede63cb13d
Petar Jovanovic <jovanovic.petar499@gmail.com>
2022-07-19 14:37:37 +0200

    Created a function to round up user data

--- lib/widgets/vertical_block.dart
+++ lib/widgets/vertical_block.dart
@@ -5,6 +5,18 @@ class VerticalBlock extends StatelessWidget {
   final String label;
   const VerticalBlock({required this.count, required this.label, Key? key}) : super(key: key);
 
+  String roundCount() {
+    if (count >= 1000 && count <= 999999) {
+      var newCount = (count / 1000).floor().toInt();
+      return "${newCount}K";
+    } else if (count >= 1000000) {
+      var newCount = (count / 1000000).floor().toInt();
+      return "${newCount}M";
+    } else {
+      return "${count.round()}";
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     TextStyle textStyle = Theme.of(context).textTheme.subtitle2!;
@@ -12,7 +24,7 @@ class VerticalBlock extends StatelessWidget {
       height: 60,
       child: Column(
         children: [
-          Text(count.toString(), style: textStyle),
+          Text(roundCount(), style: textStyle),
           Text(label, style: textStyle),
         ],
       ),
